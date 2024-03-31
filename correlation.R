63ded42aae2c316500ee46ce577ac96e6a4154ab
padmashris <psarava1@jhu.edu>
2024-03-31 13:51:01 -0400

    added IRR quantile plots

--- correlation.R
+++ correlation.R
@@ -71,6 +71,148 @@ plot_correlation <- function(param.list, param.title, simset){
   cowplot::plot_grid(plotlist = plot.list, ncol = 2)
 }
 
+# Boxplot function -----
+print_boxplot <- function(simset, city){
+  
+  ## top 25% quantile -----
+  params <- as.data.frame(simset$parameters) 
+  final_param <- print_param(simset)
+  
+  params <- params[final_param,]
+  
+  quart.3 <- apply(params, 1, quantile, probs = 0.75)
+  
+  top.25 <- data.frame(
+    name = final_param,
+    quart.3 = quart.3
+  )
+  
+  print_irr <- function(simset){
+    bh.ir <- simset$get(outcome = "incidence", 
+                        dimension.values = list(year = '2035', 
+                                                race = c("black","hispanic"),
+                                                sex = "msm"),
+                        keep.dimensions = character())/simset$get(
+                          outcome = "population", 
+                          dimension.values = list(year = '2035', 
+                                                  race = c("black","hispanic"), 
+                                                  sex = "msm"),
+                          keep.dimensions = character())
+    
+    other.ir <- simset$get(outcome = "incidence", dimension.values = list(
+      year = '2035', race = "other", 
+      sex = "msm"),
+      keep.dimensions = character())/simset$get(
+        outcome = "population", 
+        dimension.values = list(year = '2035', race = "other", 
+                                sex = "msm"),
+        keep.dimensions = character())
+    
+    irr <- bh.ir/other.ir
+    
+    return(irr)
+  }
+  
+  param_list <- list()
+  
+  for(name in final_param){
+    index <- which(params[name,] >= top.25[name,2])
+    param_list[[name]]$index <- t(params[name, index])
+    param_list[[name]]$irr <- cbind(print_irr(simset)[index])
+    param_list[[name]]$quantile <- rep("Top 25%", length(index))
+  }
+  
+  # convert param_list to a data frame
+  param_list_top25 <- lapply(param_list, function(x) as.data.frame(x))
+  
+  ## bottom 25% quantile -----
+  quart.1 <- apply(params, 1, quantile, probs = 0.25)
+  
+  bottom.25 <- data.frame(
+    name = final_param,
+    quart.1 = quart.1
+  )
+  
+  param_list <- list()
+  
+  for(name in final_param){
+    index <- which(params[name,] >= bottom.25[name,2])
+    param_list[[name]]$index <- t(params[name, index])
+    param_list[[name]]$irr <- cbind(print_irr(simset)[index])
+    param_list[[name]]$quantile <- rep("Bottom 25%", length(index))
+  }
+  
+  # convert param_list to a data frame
+  param_list_bottom25 <- lapply(param_list, function(x) as.data.frame(x))
+  
+  ## plotting -----
+  # plot boxplot
+  ggplot() + 
+    geom_boxplot(data = param_list_top25[[1]], 
+                 aes(x = irr, y = final_param[1], 
+                     group = final_param[1], fill = "Top 25%"),
+                 orientation = "y") +
+    geom_boxplot(data = param_list_bottom25[[1]],
+                 aes(x = irr, y = final_param[1], 
+                     group = final_param[1], fill = "Bottom 25%"),
+                 orientation = "y", position = position_dodge(25)) +
+    geom_boxplot(data = param_list_top25[[2]],
+                 aes(x = irr, y = final_param[2], 
+                     group = final_param[2], fill = "Top 25%"),
+                 orientation = "y") +
+    geom_boxplot(data = param_list_bottom25[[2]],
+                 aes(x = irr, y = final_param[2], 
+                     group = final_param[2], fill = "Bottom 25%"),
+                 orientation = "y") +
+    geom_boxplot(data = param_list_top25[[3]],
+                 aes(x = irr, y = final_param[3], 
+                     group = final_param[3], fill = "Top 25%"),
+                 orientation = "y") +
+    geom_boxplot(data = param_list_bottom25[[3]],
+                 aes(x = irr, y = final_param[3], 
+                     group = final_param[3], fill = "Bottom 25%"),
+                 orientation = "y") +
+    geom_boxplot(data = param_list_top25[[4]],
+                 aes(x = irr, y = final_param[4],
+                     group = final_param[4], fill = "Top 25%"),
+                 orientation = "y") +
+    geom_boxplot(data = param_list_bottom25[[4]],
+                 aes(x = irr, y = final_param[4], 
+                     group = final_param[4], fill = "Bottom 25%"),
+                 orientation = "y") +
+    geom_boxplot(data = param_list_top25[[5]],
+                 aes(x = irr, y = final_param[5], 
+                     group = final_param[5], fill = "Top 25%"),
+                 orientation = "y") +
+    geom_boxplot(data = param_list_bottom25[[5]],
+                 aes(x = irr, y = final_param[5], 
+                     group = final_param[5], fill = "Bottom 25%"),
+                 orientation = "y") +
+    geom_boxplot(data = param_list_top25[[6]],
+                 aes(x = irr, y = final_param[6], 
+                     group = final_param[6], fill = "Top 25%"),
+                 orientation = "y") +
+    geom_boxplot(data = param_list_bottom25[[6]],
+                 aes(x = irr, y = final_param[6], 
+                     group = final_param[6], fill = "Bottom 25%"),
+                 orientation = "y") +
+    geom_boxplot(data = param_list_top25[[7]],
+                 aes(x = irr, y = final_param[7], 
+                     group = final_param[7], fill = "Top 25%"),
+                 orientation = "y") +
+    geom_boxplot(data = param_list_bottom25[[7]],
+                 aes(x = irr, y = final_param[7], 
+                     group = final_param[7], fill = "Bottom 25%"),
+                 orientation = "y") +
+    labs(x = "Primary Outcome of IRR", y = "Parameter", 
+         title = paste("IRR in high vs low quintiles of each parameter in ", city),
+         fill = "Quantile of Parameter") + 
+    scale_fill_manual(values = c("Top 25%" = "orange3", "Bottom 25%" = "turquoise4")) +
+    facet_wrap(~quantile, dir = "v") +
+    theme_minimal()
+}
+
+
 # Houston
 load("../../files/simulations/ehe/init.transmission.ehe-100/C.26420/ehe_init.transmission.ehe-100_C.26420_prepu40p80msm.Rdata")
 
@@ -91,6 +233,8 @@ param.title <- c(
 )
 
 plot_correlation(print_param(simset), param.title, simset)
+
+print_boxplot(simset, "Houston")
 
 # Miami 
 
@@ -114,6 +258,9 @@ param.title <- c(
 
 plot_correlation(print_param(simset), param.title, simset)
 
+print_boxplot(simset, "Miami")
+
+
 # Chicago
 
 load("../../files/simulations/ehe/init.transmission.ehe-100/C.16980/ehe_init.transmission.ehe-100_C.16980_prepu40p80msm.Rdata")
@@ -131,7 +278,6 @@ param.title <- c(
 )
 plot_correlation(print_param(simset), param.title, simset)
 
+print_boxplot(simset)
 
-# cor(irr, simset$parameters[1,], method = "spearman")
-
-
+print_boxplot(simset, "Chicago")
